54e4c211764dd24fd2688ca8acba3815afd19429
salecharohit <i@rohitsalecha.com>
2024-10-20 19:31:54 +0530

    - changing baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://s4cpaws.rohitsalecha.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/s4cp/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
