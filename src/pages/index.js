c87d26c9d5870903e6753305bf1250740ceefc7c
salecharohit <i@rohitsalecha.com>
2024-10-20 21:53:02 +0530

    - adding description

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Securing 4 C's of a Software Product - AWS Edition by @salecharohit`}
+      description="Following a successful MVP demonstration, a startup recently secured significant funding and is preparing for a soft launch, highlighting the need for enhanced security measures. Their initial proof of concept lacks essential security standards, including proper Secrets Management and compliance protocols, which are crucial for building customer trust. In response to these challenges, we created 'Securing 4C's of Software Product,' a specialized training program designed to fortify the core pillars of product security: Code, Containers, Clusters, and Cloud. This comprehensive training covers key security domains such as Authentication and Authorization in AWS and Kubernetes, Container Security, and Static Application Security, equipping participants with the knowledge to implement robust security protocols and ensure secure deployments in today’s cloud-native environment.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
